d52f1db71ad3dae8f5a380986eba873e729d00bb
Vikms95 <vms1995@hotmail.com>
2023-08-31 23:22:11 +0200

    feat: :sparkles: link client server db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,60 +2,60 @@ import { Component } from '@angular/core';
 import { TodoService } from './services/todo.service';
 import { ITodo } from './interfaces';
 
-@Component( {
+@Component({
   selector: 'app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-} )
+  styleUrls: ['./app.component.css'],
+})
 export class AppComponent {
-  constructor ( private todoService: TodoService ) {
-    todoService
-      .getTodos()
-      .subscribe( data => this.todoArray = data );
+  constructor(private todoService: TodoService) {
+    todoService.getTodos().subscribe((data) => {
+      console.log(data);
+      this.todoArray = data;
+      console.log(this.todoArray);
+    });
   }
 
-  todoId = 3;
+  todoId = Math.random() * 0.5;
   todoArray: ITodo[] = [];
 
-
-  addTodo ( value: string, taskInput: HTMLInputElement ) {
+  addTodo(value: string, taskInput: HTMLInputElement) {
     const todo = {
       id: this.todoId,
       title: value,
       done: false,
-      forDeletion: false
+      forDeletion: false,
     };
 
-    this.todoService.addTodo( todo ).subscribe( () =>
-      this.todoArray.push( todo )
-    );
+    this.todoService.addTodo(todo).subscribe(() => this.todoArray.push(todo));
 
     this.todoId++;
     taskInput.value = '';
   }
 
-  restoreForDeletion () {
-    document.getElementById( 'delete-confirm' )!.style.display = 'none';
+  restoreForDeletion() {
+    document.getElementById('delete-confirm')!.style.display = 'none';
 
-    this.todoArray.forEach( todo => todo.forDeletion = false );
-
+    this.todoArray.forEach((todo) => (todo.forDeletion = false));
   }
 
-  deleteTodo () {
-    document.getElementById( 'delete-confirm' )!.style.display = 'none';
+  deleteTodo() {
+    document.getElementById('delete-confirm')!.style.display = 'none';
 
-    const todo = this.todoArray.find( todo => {
+    const todo = this.todoArray.find((todo) => {
       return todo.forDeletion === true ? todo : null;
-    } );
+    });
 
-    if ( todo ) this.todoService
-      .deleteTodo( todo )
-      .subscribe( () =>
-        this.todoArray = this.todoArray.filter( t => t.id !== todo.id ) );
+    if (todo)
+      this.todoService
+        .deleteTodo(todo)
+        .subscribe(
+          () =>
+            (this.todoArray = this.todoArray.filter((t) => t.id !== todo.id))
+        );
   }
 
-  clearTodos () {
-    this.todoArray.splice( 0 );
+  clearTodos() {
+    this.todoArray.splice(0);
   }
-
 }
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,32 +3,31 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ITodo } from '../interfaces';
 import { Observable, of } from 'rxjs';
 
-@Injectable( {
-  providedIn: 'root'
-} )
+@Injectable({
+  providedIn: 'root',
+})
 export class TodoService {
-  constructor ( private http: HttpClient ) { }
+  constructor(private http: HttpClient) {}
 
-  private url = 'http://localhost:5000/data/';
+  private url = 'http://localhost:5000';
 
-  getTodos (): Observable<ITodo[]> {
-    return this.http.get<ITodo[]>( this.url );
+  getTodos(): Observable<ITodo[]> {
+    return this.http.get<ITodo[]>(this.url + '/todo');
   }
 
-  addTodo ( todo: ITodo ): Observable<ITodo> {
-    return this.http.post<ITodo>( this.url, todo );
+  addTodo(todo: ITodo): Observable<ITodo> {
+    return this.http.post<ITodo>(this.url + '/todo', todo);
   }
 
-  markAsDone ( todo: ITodo ): Observable<ITodo> {
+  markAsDone(todo: ITodo): Observable<ITodo> {
     const url = this.url + todo.id;
     const body = todo;
 
-    return this.http.put<ITodo>( url, body );
+    return this.http.put<ITodo>(url, body);
   }
 
-  deleteTodo ( todo: ITodo ): Observable<ITodo> {
+  deleteTodo(todo: ITodo): Observable<ITodo> {
     const url = this.url + todo.id;
-    return this.http.delete<ITodo>( url );
+    return this.http.delete<ITodo>(url);
   }
-
 }
